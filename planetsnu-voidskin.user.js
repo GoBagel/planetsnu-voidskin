c4bf5bbfb4b63f7dfdbe5f62827c95efd9007251
Mo <maurice.bujold@tronicdata.com>
2026-05-02 05:43:05 -0600

    Bump version to 0.0.5 in user script metadata and internal constant

--- planetsnu-voidskin.user.js
+++ planetsnu-voidskin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Planets.nu – VoidSkin Galactic Background
 // @namespace    bagelman/nu/voidskin
-// @version      0.0.4
+// @version      0.0.5
 // @description  Customize the Planets.nu starmap background with static, parallax star, and comet layers.
 // @author       GoBagel + ChatGPT
 // @match        https://planets.nu/*
@@ -23,7 +23,7 @@
 function wrapper() {
   "use strict";
 
-  const VERSION = "0.0.4";
+  const VERSION = "0.0.5";
   const STORAGE_KEY = "planetsnu_voidskin_settings";
 
   const DEFAULT_SETTINGS = {
